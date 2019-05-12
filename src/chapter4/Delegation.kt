8cf6d261a808ea1686dfda28944b1aed3ed5755a
leejinseong <dlwls5201@naver.com>
2019-05-12 14:42:01 +0900

    add deegarion and readme04

---
+++ src/chapter4/Delegation.kt
@@ -0,0 +1,56 @@
+package chapter4
+
+// C를 생성하고, A에서 정의하는 B의 모든 메서드를 C에 위임합니다.
+// class C : A by b
+
+
+interface Base {
+    fun printX()
+}
+
+class BaseImpl(val x: Int) : Base {
+    override fun printX() { print(x) }
+
+    private var y : Int = 100
+
+    fun printY() { print(y) }
+}
+val baseImpl = BaseImpl(10)
+
+//Derived 를 생성하고 Base 에서 정의하는 baseImpl 의 모든 메소드를 Derived 에 위임하겠다.
+class Derived(baseImpl: Base) : Base by baseImpl
+
+
+
+
+
+
+interface Vehicle {
+    fun go(): String
+}
+class CarImpl(val where: String): Vehicle {
+    override fun go() = "is going to $where"
+}
+class AirplaneImpl(val where: String): Vehicle {
+    override fun go() = "is flying to $where"
+}
+class CarOrAirplane(
+    val model: String,
+    impl: Vehicle
+): Vehicle by impl {
+    fun tellMeYourTrip() {
+        println("$model ${go()}")
+    }
+}
+
+fun main() {
+
+    val myAirbus330
+            = CarOrAirplane("Lamborghini", CarImpl("Seoul"))
+    val myBoeing337
+            = CarOrAirplane("Boeing 337", AirplaneImpl("Seoul"))
+
+    myAirbus330.tellMeYourTrip()
+    myBoeing337.tellMeYourTrip()
+
+}
